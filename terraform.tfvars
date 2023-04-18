c0e891b32f2aeb2b434adb11d95dab01ff334684
David Vanyan <101475933+DVanyan@users.noreply.github.com>
2023-04-18 21:01:10 +0400

    AAA

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,4 @@
-project_id = "storied-scarab-383615"
+gcp_project = "storied-scarab-383615"
+region = "us-east1"
+name = "david"
+subnet_cidr = "10.10.0.0/24"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east1"
+}
+
+variable "gcp_project" {
+  default = ""
+}
+
+variable "name"{
+  default = "deployment"
+}
+
+variable "subnet_cidr"{
+  default = "0.0.0.0/32"
+}
