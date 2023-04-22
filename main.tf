b973481931268052b8cd484995e388309ba6fd95
DVanyan <davidvanyan@gmail.com>
2023-04-22 13:49:20 +0400

    test

--- main.tf
+++ main.tf
@@ -1,9 +1,44 @@
-data "github_actions_public_key" "secret_cloud_token" {
-  repository = "GKEcreate"
+resource "google_compute_network" "vpc" {
+  name                    = "${var.name}-vpc"
+  auto_create_subnetworks = "false"
 }
 
-resource "github_actions_secret" "tf_secret" {
-  repository       = "GKEcreate"
-  secret_name      = "TF_CLOUD_TOKEN"
-  plaintext_value  = var.tf_secret
+resource "google_compute_subnetwork" "subnet" {
+  depends_on = [
+    google_compute_network.vpc
+  ]
+
+  name          = "${var.name}-subnet"
+  ip_cidr_range = var.subnet_cidr
+  network       = "${var.name}-vpc"
+  region        = var.region
+}
+// VPC firewall configuration
+resource "google_compute_firewall" "firewall" {
+  depends_on = [
+    google_compute_subnetwork.subnet
+  ]
+  name    = "${var.name}-firewall"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
